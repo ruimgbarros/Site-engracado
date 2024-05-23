6b3fb3bd40695e5676a8ebccebeb9d8c46e55aa8
ruimgbarros <ruimgbarros@gmail.com>
2024-05-24 00:06:26 +0100

    refactor: Update server.js to serve static files and fix file paths

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const clientId = '11a73fdbec8b4e8e837eae1be557ec6e'; // Your Spotify client ID
 const clientSecret = '8a49af646d2346e68287e2c9345c15a5'; // Your Spotify client secret
 const redirectUri = 'https://site-engracado-beta.vercel.app/callback'; // Update with your Vercel deployment URL
 
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '/')));
+
 // Serve the index.html file at the root URL
 app.get('/', (req, res) => {
-    // Render index.html and pass clientId and redirectUri as template variables
-    res.sendFile(path.join(__dirname, 'index.html'), {
-        clientId: clientId,
-        redirectUri: redirectUri
-    });
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/callback', async (req, res) => {
@@ -46,7 +45,7 @@ app.get('/callback', async (req, res) => {
         // Store tokens or handle them as needed
         res.send(`Access Token: ${access_token}<br>Refresh Token: ${refresh_token}`);
     } catch (error) {
-        if(!res.headersSent) {
+        if (!res.headersSent) {
             res.status(500).send('Error retrieving access token.');
         }
     }
@@ -56,14 +55,10 @@ app.get('/game.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'game.html'));
 });
 
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/background.mp4.mp4', (req, res) => {
-    res.sendFile(path.join(__dirname, 'background.mp4.mp4'));
-});
-
 app.get('/game_over.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'game_over.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
