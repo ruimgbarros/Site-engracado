51586506097e25103f23d6dc476875c4de8e11d4
ruimgbarros <ruimgbarros@gmail.com>
2024-05-23 22:57:55 +0100

    refactor: Update redirectUri for production deployment

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const axios = require('axios');
 const querystring = require('querystring');
@@ -9,7 +8,7 @@ const port = process.env.PORT || 8000;
 
 const clientId = '11a73fdbec8b4e8e837eae1be557ec6e'; // Your Spotify client ID
 const clientSecret = '8a49af646d2346e68287e2c9345c15a5'; // Your Spotify client secret
-const redirectUri = process.env.PORT ? `https://genrefy-psi.vercel.app/` : 'http://localhost:8000/callback';
+const redirectUri = 'https://genrefy-psi.vercel.app/callback'; // Update with your Vercel deployment URL
 
 // Serve the index.html file at the root URL
 app.get('/', (req, res) => {
